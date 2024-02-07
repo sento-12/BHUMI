65219bfb765b26d3b4d20d1e265a2e2e1a3b22f0
Manvendrashih <vishvajitherma@gmail.com>
2024-02-07 18:54:30 +0530

    form finish

---
+++ form.js
@@ -0,0 +1,16 @@
+let not_a_number1 = document.querySelector("#nan2");
+
+// function number2(){
+//     if(!Number(not_a_number1)){
+//         document.getElementById("mag-nan2").innerHTML = "Enter Only Number";        
+//     }
+//     else{
+//         document.getElementById("mag-nan2").innerHTML 
+//     }
+// }
+
+function number () {
+    if(!Number(not_a_number1)){
+        document.querySelector("#msg-nan").innerHTML = "Enter Only Number";
+    }
+}
