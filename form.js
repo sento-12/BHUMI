3d9eeb063de54f07316bae02cae52f6e8517c7d6
Manvendrashih <vishvajitherma@gmail.com>
2024-02-08 12:01:46 +0530

    form js

--- form.js
+++ form.js
@@ -4,7 +4,7 @@ function checkNumber() {
 
     // Check if it's a number
     if (!isNaN(userInput)) {
-        console.log(document.getElementById('nan2').value)
+        console.log(document.getElementById('nan2').value);
     } else {
         document.getElementById('msg-nan').innerText =  'Enter Only Number After Refresh';
     }
