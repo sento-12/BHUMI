576cdd88d511b3352374cb2d9d065caaf4e04086
Manvendrashih <vishvajitherma@gmail.com>
2024-02-10 14:41:14 +0530

    complite for rate

--- script.js
+++ script.js
@@ -1,3 +1,49 @@
+// rate of bag
+
+
+document.getElementById("keser60").addEventListener("click", e => {
+    document.getElementById("rate60").style.display = "block";
+    document.getElementById("rate50").style.display = "none";
+})
+
+document.querySelector("#keser50").addEventListener("click", e => {
+    document.getElementById("rate50").style.display = "block";
+    document.getElementById("rate60").style.display = "none";
+})
+
+
+
+// box 2
+
+document.getElementById("paghadi50").addEventListener("click", e => {
+    document.getElementById("rate-paghadi50").style.display = "block";
+    document.getElementById("rate-paghadi60").style.display = "none";
+})
+
+
+document.getElementById("paghadi60").addEventListener("click", e => {
+    document.getElementById("rate-paghadi60").style.display = "block";
+    document.getElementById("rate-paghadi50").style.display = "none";
+})
+
+
+// box 3
+
+document.getElementById("vrundavan50").addEventListener("click", e => {
+    document.getElementById("rate-vrundavan50").style.display = "block";
+    document.getElementById("rate-vrundavan60").style.display = "none";
+})
+
+
+document.getElementById("vrundavan60").addEventListener("click", e => {
+    document.getElementById("rate-vrundavan60").style.display = "block";
+    document.getElementById("rate-vrundavan50").style.display = "none";
+})
+
+
+
+
+
 let button = document.querySelector(".button").addEventListener("click", e =>{
 console.log("Starting")
     
