39f3794fb48b297ea514d995d379c072e6b045ce
sento <152507238+sento-12@users.noreply.github.com>
2024-03-15 18:28:59 +0530

    all file are wasted code and item are deleted for new method applie

--- form.js
+++ form.js
@@ -1,27 +1,4 @@
-<<<<<<< HEAD
-function checkNumber() {
-    // Get user input
-    var userInput = document.getElementById('nan2').value;
 
-    // Check if it's a number
-    if (!isNaN(userInput)) {
-        console.log(document.getElementById('nan2').value);
-    } else {
-        document.getElementById('msg-nan').innerText =  'Enter Only Number After Refresh';
-    }
-
-
-    // LOCAL HPST ChannelMergerNode.apply.apply.apply.apply.apply.apply.apply.apply.
-
-}
-
-
- console.log("--------- this working... -->");
-
-
-
-
-=======
 function checkNumber() {
   // Get user input
   var userInput = document.getElementById("nan2").value;
@@ -36,28 +13,10 @@ function checkNumber() {
 
   // LOCAL HPST ChannelMergerNode.apply.apply.apply.apply.apply.apply.apply.apply.
 
-  let name = document.getElementById("partyName").value;
-  localStorage.setItem("name", name);
-
-  let phoneNumber = document.getElementById("phoneNumber").value;
-  localStorage.setItem("phoneNumber", phoneNumber);
-
-  let gstNumber = document.getElementById("gstNumber").value;
-  localStorage.setItem("gstNumber", gstNumber);
-
-  let PANcard = document.getElementById("PANcard").value;
-  localStorage.setItem("PANcard", PANcard);
-
-  let city = document.getElementById("city").value;
-  localStorage.setItem("city", city);
-
-  let marko = document.getElementById("box").value;
-  localStorage.setItem("marko", marko);
-
-  console.log(name, gstNumber, city, marko);
 }
 
 let html = `
+
 <div id="size">
           <div class="input-box">
             <span > BAG</span>
@@ -94,4 +53,3 @@ document.querySelector(".symbol").addEventListener("click", () => {
 });
 
 console.log("--------- this working... -->");
->>>>>>> 44e1b026ba6bba378b61094fe34004ae0aaeee2e
