d87058b3bda9c5e882a11a17579f254f6d2d4c22
sento <152507238+sento-12@users.noreply.github.com>
2024-03-15 18:12:43 +0530

    nooo idea.

--- form.js
+++ form.js
@@ -12,27 +12,6 @@ function checkNumber() {
 
     // LOCAL HPST ChannelMergerNode.apply.apply.apply.apply.apply.apply.apply.apply.
 
-
-    let name = document.getElementById("partyName").value;
-    localStorage.setItem('name', name);
-
-    let phoneNumber = document.getElementById("phoneNumber").value;
-    localStorage.setItem('phoneNumber', phoneNumber);
-
-    let gstNumber = document.getElementById("gstNumber").value;
-    localStorage.setItem('gstNumber', gstNumber);
-
-    let PANcard = document.getElementById("PANcard").value;
-    localStorage.setItem('PANcard', PANcard);
-   
-    let city = document.getElementById("city").value;
-    localStorage.setItem('city', city);
-
-    let marko = document.getElementById("box").value;
-    localStorage.setItem('marko', marko);
-
-
-    console.log(name , gstNumber , city , marko);
 }
 
 
